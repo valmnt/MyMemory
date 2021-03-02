d9562c7e8b0b92971a0bcc9ba5fa88191eb33d46
MONT <valentin.mont@ynov.com>
2021-03-02 10:00:19 +0100

    new theme

--- app/src/main/java/com/rkpandey/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/rkpandey/mymemory/MemoryBoardAdapter.kt
@@ -63,7 +63,7 @@ class MemoryBoardAdapter(
           imageButton.setImageResource(memoryCard.identifier)
         }
       } else {
-        imageButton.setImageResource(R.drawable.bamboo)
+        imageButton.setImageResource(R.drawable.halloween)
       }
       imageButton.alpha = if (memoryCard.isMatched) .4f else 1f
       val colorStateList = if (memoryCard.isMatched) ContextCompat.getColorStateList(context, R.color.color_gray) else null
